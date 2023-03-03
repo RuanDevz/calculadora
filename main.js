57a51eba3b7ef722edd3a5998b87fe4ae81e76ef
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-03-03 11:29:29 -0300

    calc

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
-// selecione o elemento de resultado da calculadora
 const resultado = document.querySelector('#resultado');
-
-// selecione todos os botões da calculadora
 const botoes = document.querySelectorAll('button');
-
-// variável para armazenar o resultado atual da calculadora
 let resultadoAtual = '';
+let sinal = '+';
 
-// loop através de cada botão e adiciona um evento de clique a cada um
 botoes.forEach(botao => {
   botao.addEventListener('click', () => {
-    // obtém o valor do botão clicado
     const valorBotao = botao.innerText;
 
     if (valorBotao === 'C') {
-      // se o botão "C" for clicado, apaga todo o resultado
       resultadoAtual = '';
       resultado.value = '';
+      sinal = '+';
     } else if (valorBotao === 'CE') {
-      // se o botão "CE" for clicado, apaga o último caractere do resultado
       resultadoAtual = resultadoAtual.slice(0, -1);
       resultado.value = resultadoAtual;
     } else if (valorBotao === '=') {
-      // se o botão "=" for clicado, realiza o cálculo e exibe o resultado
-      resultado.value = eval(resultadoAtual);
+      const valores = resultadoAtual.split(sinal);
+      let soma = 0;
+      if (sinal === '+') {
+        soma = valores.reduce((total, valor) => total + parseFloat(valor), 0);
+      } else if (sinal === '-') {
+        soma = valores.reduce((total, valor) => total - parseFloat(valor), parseFloat(valores.shift()));
+      } else if (sinal === 'x') {
+        soma = valores.reduce((total, valor) => total * parseFloat(valor), 1);
+      } else if (sinal === '/') {
+        soma = valores.reduce((total, valor) => total / parseFloat(valor), parseFloat(valores.shift()));
+      }
+      resultado.value = soma;
+      resultadoAtual = soma.toString();
+      sinal = '+';
+    } else if (valorBotao === '+' || valorBotao === '-' || valorBotao === 'x' || valorBotao === '/') {
+      sinal = valorBotao;
+      resultadoAtual += valorBotao;
+      resultado.value = resultadoAtual;
     } else {
-      // caso contrário, adiciona o valor do botão ao resultado atual
       resultadoAtual += valorBotao;
       resultado.value = resultadoAtual;
     }
